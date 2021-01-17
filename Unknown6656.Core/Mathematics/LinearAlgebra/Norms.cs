f930909f61d1f25711586dc9e144d67a2f78deea
unknown6656 <unknown84@gmx.de>
2021-01-18 00:17:18 +0100

    added defintitions for vector/matrix norms

---
+++ Unknown6656.Core/Mathematics/LinearAlgebra/Norms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Mathematics.LinearAlgebra
+{
+    public enum VectorNorm
+    {
+        EucledianNorm,
+        TaxicabNorm,
+        MaximumNorm,
+        ManhattanNorm = TaxicabNorm,
+        Linf_Norm = MaximumNorm,
+    }
+
+    public enum MatrixNorm
+    {
+        EucledianNorm,
+        FrobeniusNorm = EucledianNorm,
+        L21_Norm,
+        // MaxNorm,
+        L1_Norm,
+        Linf_Norm,
+        L2_Norm,
+        SpectralNorm = L2_Norm,
+    }
+}

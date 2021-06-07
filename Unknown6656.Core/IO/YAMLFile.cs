fca45663b5efec879c983c81ab65028be6ae166c
unknown6656 <unknown84@gmx.de>
2021-06-07 10:50:59 +0200

    began adding YAML support

---
+++ Unknown6656.Core/IO/YAMLFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.IO
+{
+    public abstract class YAMLObject
+        : DynamicObject
+        , ICloneable
+    {
+    }
+
+    public sealed class YAMLValue
+        : YAMLObject
+    {
+    }
+
+    public class YAMLSection
+        : YAMLObject
+        , IDictionary<string, YAMLObject>
+    {
+    }
+
+    public sealed class YAMLFile
+        : YAMLSection
+    {
+    }
+}

fa66e69b0a527650ffb06542032d29bc5e788e2d
unknown6656 <unknown84@gmx.de>
2022-02-12 12:54:19 +0100

    removed even more legacy code

--- Unknown6656.Core/Common/LINQ.cs
+++ Unknown6656.Core/Common/LINQ.cs
@@ -13,6 +13,7 @@ using static System.Math;
 
 namespace Unknown6656.Common;
 
+
 public static class LINQExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -29,38 +30,4 @@ public static class LINQExtensions
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe U BinaryCast<T, U>(this T value) where T : unmanaged where U : unmanaged => *(U*)&value;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe U[] CopyTo<T, U>(this T[] source) where T : unmanaged where U : unmanaged => source.CopyTo<T, U>(sizeof(T) * source.Length);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe U[] CopyTo<T, U>(this T[] source, int byte_count)
-        where T : unmanaged
-        where U : unmanaged
-    {
-        U[] dest = new U[(int)Ceiling(byte_count / (float)sizeof(U))];
-
-        fixed (U* ptrd = dest)
-            source.CopyTo(ptrd, byte_count);
-
-        return dest;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void CopyTo<T, U>(this T[] source, U* target) where T : unmanaged where U : unmanaged => source.CopyTo(target, sizeof(T) * source.Length);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void CopyTo<T, U>(this T[] source, U* target, int byte_count)
-        where T : unmanaged
-        where U : unmanaged
-    {
-        fixed (T* ptrs = source)
-        {
-            byte* bptrd = (byte*)target;
-            byte* bptrs = (byte*)ptrs;
-
-            for (int i = 0; i < byte_count; ++i)
-                bptrd[i] = bptrs[i];
-        }
-    }
 }

4b30cddd13b532bed4cae6ec77c244bbae61427d
unknown6656 <unknown84@gmx.de>
2022-02-12 12:47:54 +0100

    moved to file-scoped namespaces

--- Unknown6656.Core/IO/YAMLFile.cs
+++ Unknown6656.Core/IO/YAMLFile.cs
@@ -8,93 +8,93 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unknown6656.IO
+namespace Unknown6656.IO;
+
+
+public abstract class YAMLObject
+    : DynamicObject
+    , ICloneable
 {
-    public abstract class YAMLObject
-        : DynamicObject
-        , ICloneable
+    public abstract override IEnumerable<string> GetDynamicMemberNames();
+    public abstract override DynamicMetaObject GetMetaObject(Expression parameter);
+    public abstract override bool TryDeleteIndex(DeleteIndexBinder binder, object[] indexes);
+    public abstract override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object? result);
+    public abstract override bool TryDeleteMember(DeleteMemberBinder binder);
+    public abstract override bool TryGetMember(GetMemberBinder binder, out object? result);
+    public abstract override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object? value);
+    public abstract override bool TrySetMember(SetMemberBinder binder, object? value);
+}
+
+public sealed class YAMLValue
+    : YAMLObject
+{
+    public object? Value { get; }
+    public YAMLValueType Type { get; }
+
+
+}
+
+public sealed class YAMLArray
+    : YAMLObject
+    , IList<YAMLObject>
+{
+}
+
+public class YAMLSection
+    : YAMLObject
+    , IDictionary<string, YAMLObject>
+{
+    private readonly Dictionary<string, YAMLObject> _dictionary = new();
+
+
+    public YAMLObject this[string key]
     {
-        public abstract override IEnumerable<string> GetDynamicMemberNames();
-        public abstract override DynamicMetaObject GetMetaObject(Expression parameter);
-        public abstract override bool TryDeleteIndex(DeleteIndexBinder binder, object[] indexes);
-        public abstract override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object? result);
-        public abstract override bool TryDeleteMember(DeleteMemberBinder binder);
-        public abstract override bool TryGetMember(GetMemberBinder binder, out object? result);
-        public abstract override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object? value);
-        public abstract override bool TrySetMember(SetMemberBinder binder, object? value);
+        get => _dictionary[key];
+        set => _dictionary[key] = value;
     }
 
-    public sealed class YAMLValue
-        : YAMLObject
-    {
-        public object? Value { get; }
-        public YAMLValueType Type { get; }
+    public ICollection<string> Keys => _dictionary.Keys;
+
+    public ICollection<YAMLObject> Values => _dictionary.Values;
+
+    public int Count => _dictionary.Count;
+
+    public bool IsReadOnly => false;
 
 
-    }
+    public void Add(string key, YAMLObject value) => _dictionary.Add(key, value);
 
-    public sealed class YAMLArray
-        : YAMLObject
-        , IList<YAMLObject>
-    {
-    }
+    public void Add(KeyValuePair<string, YAMLObject> item) => _dictionary.Add(item.Key, item.Value);
 
-    public class YAMLSection
-        : YAMLObject
-        , IDictionary<string, YAMLObject>
-    {
-        private readonly Dictionary<string, YAMLObject> _dictionary = new();
+    public void Clear() => _dictionary.Clear();
 
+    public bool Contains(KeyValuePair<string, YAMLObject> item) => _dictionary.Contains(item);
 
-        public YAMLObject this[string key]
-        {
-            get => _dictionary[key];
-            set => _dictionary[key] = value;
-        }
+    public bool ContainsKey(string key) => _dictionary.ContainsKey(key);
 
-        public ICollection<string> Keys => _dictionary.Keys;
+    public bool Remove(string key) => _dictionary.Remove(key);
 
-        public ICollection<YAMLObject> Values => _dictionary.Values;
+    public bool Remove(KeyValuePair<string, YAMLObject> item) => _dictionary.Remove(item.Key);
 
-        public int Count => _dictionary.Count;
+    public bool TryGetValue(string key, [MaybeNullWhen(false)] out YAMLObject value) => _dictionary.TryGetValue(key, out value);
 
-        public bool IsReadOnly => false;
+    void ICollection<KeyValuePair<string, YAMLObject>>.CopyTo(KeyValuePair<string, YAMLObject>[] array, int arrayIndex) => ((ICollection<KeyValuePair<string, YAMLObject>>)_dictionary).CopyTo(array, arrayIndex);
 
+    IEnumerator<KeyValuePair<string, YAMLObject>> IEnumerable<KeyValuePair<string, YAMLObject>>.GetEnumerator() => ((IEnumerable<KeyValuePair<string, YAMLObject>>)_dictionary).GetEnumerator();
 
-        public void Add(string key, YAMLObject value) => _dictionary.Add(key, value);
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_dictionary).GetEnumerator();
+}
 
-        public void Add(KeyValuePair<string, YAMLObject> item) => _dictionary.Add(item.Key, item.Value);
+public sealed class YAMLDocument
+    : YAMLSection
+{
+}
 
-        public void Clear() => _dictionary.Clear();
-
-        public bool Contains(KeyValuePair<string, YAMLObject> item) => _dictionary.Contains(item);
-
-        public bool ContainsKey(string key) => _dictionary.ContainsKey(key);
-
-        public bool Remove(string key) => _dictionary.Remove(key);
-
-        public bool Remove(KeyValuePair<string, YAMLObject> item) => _dictionary.Remove(item.Key);
-
-        public bool TryGetValue(string key, [MaybeNullWhen(false)] out YAMLObject value) => _dictionary.TryGetValue(key, out value);
-
-        void ICollection<KeyValuePair<string, YAMLObject>>.CopyTo(KeyValuePair<string, YAMLObject>[] array, int arrayIndex) => ((ICollection<KeyValuePair<string, YAMLObject>>)_dictionary).CopyTo(array, arrayIndex);
-
-        IEnumerator<KeyValuePair<string, YAMLObject>> IEnumerable<KeyValuePair<string, YAMLObject>>.GetEnumerator() => ((IEnumerable<KeyValuePair<string, YAMLObject>>)_dictionary).GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_dictionary).GetEnumerator();
-    }
-
-    public sealed class YAMLDocument
-        : YAMLSection
-    {
-    }
-
-    public enum YAMLValueType
-    {
-        String,
-        Float,
-        Integer,
-        Boolean,
-        Binary,
-    }
+public enum YAMLValueType
+{
+    String,
+    Float,
+    Integer,
+    Boolean,
+    Binary,
 }

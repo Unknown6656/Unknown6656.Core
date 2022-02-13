17aacae92fcd6b40235eab70eaecc043ca2c3c62
unknown6656 <unknown84@gmx.de>
2022-02-13 01:18:23 +0100

    refactorings

--- Unknown6656.Core/IO/FileSystemExtensions.cs
+++ Unknown6656.Core/IO/FileSystemExtensions.cs
@@ -5,62 +5,59 @@ using System.IO;
 using Unknown6656.Mathematics.Graphs;
 using Unknown6656.Common;
 
-namespace Unknown6656.IO
+namespace Unknown6656.IO;
+
+
+public static unsafe class FileSystemExtensions
 {
-    public static unsafe class FileSystemExtensions
+    // TODO : all extensions from 'tabbedexplorer'
+
+
+    public static string GetShortPath(string path)
     {
-        // TODO : all extensions from 'tabbedexplorer'
+        StringBuilder sb = new(256);
 
+        NativeInterop.GetShortPathName(path, sb, sb.Capacity);
 
-        public static string GetShortPath(string path)
+        return sb.ToString();
+    }
+
+    public static FileSystemInfo GetSystemInfo(string path)
+    {
+        try
         {
-            StringBuilder sb = new(256);
-
-            NativeInterop.GetShortPathName(path, sb, sb.Capacity);
-
-            return sb.ToString();
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+                return new DirectoryInfo(path);
+        }
+        catch
+        {
         }
 
-        public static FileSystemInfo GetSystemInfo(string path)
-        {
-            try
-            {
-                if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
-                    return new DirectoryInfo(path);
-            }
-            catch
-            {
-            }
+        return new FileInfo(path);
+    }
 
-            return new FileInfo(path);
-        }
+    public static FileSystemInfo[] ResolveWildCards(string path_with_wildcards)
+    {
+        string dir = Path.GetDirectoryName(path_with_wildcards) ?? ".";
+        string file = Path.GetFileName(path_with_wildcards);
+        DirectoryInfo d = new(dir);
 
-        public static FileSystemInfo[] ResolveWildCards(string path_with_wildcards)
-        {
-            string dir = Path.GetDirectoryName(path_with_wildcards) ?? ".";
-            string file = Path.GetFileName(path_with_wildcards);
-            DirectoryInfo d = new(dir);
+        return d.EnumerateFileSystemInfos(file).ToArray();
+    }
 
-            return d.EnumerateFileSystemInfos(file).ToArray();
-        }
+    public static bool CreateNTFSHardLink(string link_name, string target) => NativeInterop.CreateHardLink(link_name, target, null);
 
-        public static bool CreateNTFSHardLink(string link_name, string target) => NativeInterop.CreateHardLink(link_name, target, null);
+    public static Tree<FileSystemInfo> GetDirectoryTree(string directory_path) => GetDirectoryTree(new DirectoryInfo(directory_path));
 
-        public static Tree<FileSystemInfo> GetDirectoryTree(string directory_path) => GetDirectoryTree(new DirectoryInfo(directory_path));
+    public static Tree<FileSystemInfo> GetDirectoryTree(DirectoryInfo directory) => throw new System.NotImplementedException(); // TODO
 
-        public static Tree<FileSystemInfo> GetDirectoryTree(DirectoryInfo directory)
-        {
-            throw new System.NotImplementedException(); // TODO
-        }
+    public static DirectoryInfo GetTemporaryDirectory()
+    {
+        DirectoryInfo dir = new(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
 
-        public static DirectoryInfo GetTemporaryDirectory()
-        {
-            DirectoryInfo dir = new(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+        if (!dir.Exists)
+            dir.Create();
 
-            if (!dir.Exists)
-                dir.Create();
-
-            return dir;
-        }
+        return dir;
     }
 }

5c0035cefafa43f4d1d7f34c5d30980616c53b2f
unknown6656 <unknown84@gmx.de>
2022-02-20 11:05:17 +0100

    used fie-scoped namespaces

--- Unknown6656.Core/Mathematics/LinearAlgebra/Norms.cs
+++ Unknown6656.Core/Mathematics/LinearAlgebra/Norms.cs
@@ -1,23 +1,23 @@
-﻿namespace Unknown6656.Mathematics.LinearAlgebra
-{
-    public enum VectorNorm
-    {
-        EucledianNorm,
-        TaxicabNorm,
-        MaximumNorm,
-        ManhattanNorm = TaxicabNorm,
-        Linf_Norm = MaximumNorm,
-    }
+﻿namespace Unknown6656.Mathematics.LinearAlgebra;
 
-    public enum MatrixNorm
-    {
-        EucledianNorm,
-        FrobeniusNorm = EucledianNorm,
-        L21_Norm,
-        // MaxNorm,
-        L1_Norm,
-        Linf_Norm,
-        L2_Norm,
-        SpectralNorm = L2_Norm,
-    }
+
+public enum VectorNorm
+{
+    EucledianNorm,
+    TaxicabNorm,
+    MaximumNorm,
+    ManhattanNorm = TaxicabNorm,
+    Linf_Norm = MaximumNorm,
+}
+
+public enum MatrixNorm
+{
+    EucledianNorm,
+    FrobeniusNorm = EucledianNorm,
+    L21_Norm,
+    // MaxNorm,
+    L1_Norm,
+    Linf_Norm,
+    L2_Norm,
+    SpectralNorm = L2_Norm,
 }

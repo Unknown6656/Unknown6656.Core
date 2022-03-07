10f8f96458e924b6de78ea698e48bb8ef772be4c
unknown6656 <unknown84@gmx.de>
2022-03-07 11:48:16 +0100

    git is being stupid again

---
+++ Unknown6656.Core/Physics/Units/Units.cs
@@ -0,0 +1,22 @@
+﻿using Unknown6656.Mathematics.LinearAlgebra;
+
+namespace Unknown6656.Physics.Units;
+
+
+public interface IUnit<Unit>
+    where Unit : IUnit<Unit>
+{
+    static abstract string Name { get; }
+
+    // TODO
+}
+
+public interface IMeasurable<Scalar, Unit>
+    where Scalar : IScalar<Scalar>
+    where Unit : IUnit<Unit>
+{
+    // TODO
+}
+
+
+// TODO

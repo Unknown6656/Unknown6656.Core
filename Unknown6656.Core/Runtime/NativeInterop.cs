736143e15cffec98f7ff37e32e2665b43a6d8ba6
unknown6656 <unknown84@gmx.de>
2022-03-13 10:08:46 +0100

    added platform annotations to native functions

--- Unknown6656.Core/Runtime/NativeInterop.cs
+++ Unknown6656.Core/Runtime/NativeInterop.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using System.Text;
 
 using Unknown6656.Controls.Console;
@@ -8,39 +9,62 @@ namespace Unknown6656.Runtime;
 
 internal static unsafe class NativeInterop
 {
+    [DllImport("ntdll.dll")]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern int RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool _);
+
+    [DllImport("ntdll.dll")]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern int NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, void* Parameters, uint ValidResponseOption, out uint _);
+
     [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern bool CreateHardLink([MarshalAs(UnmanagedType.LPWStr)]  string lpFileName, [MarshalAs(UnmanagedType.LPWStr)] string lpExistingFileName, void* reserved);
 
     [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern int GetShortPathName([MarshalAs(UnmanagedType.LPWStr)] string? path, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder @short, int length);
 
     [DllImport("gdi32.dll", CharSet = CharSet.Auto, EntryPoint = "GetCurrentObject", ExactSpelling = true, SetLastError = true)]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern nint IntGetCurrentObject(HandleRef hDC, nint uObjectType);
 
     [DllImport("kernel32.dll", SetLastError = true)]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern void* GetStdHandle(int nStdHandle);
 
     [DllImport("kernel32.dll")]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern bool SetConsoleMode(void* hWnd, ConsoleMode dwMode);
 
     [DllImport("kernel32.dll")]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern bool GetConsoleMode(void* hWnd, ConsoleMode* lpMode);
 
     [DllImport("kernel32.dll")]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern void* VirtualAlloc(void* addr, int size, int type, int protect);
 
     [DllImport("kernel32.dll")]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern bool VirtualProtect(void* addr, int size, int new_protect, int* old_protect);
 
     [DllImport("kernel32.dll")]
+    [SupportedOSPlatform(OS.WIN)]
     public static extern bool VirtualFree(void* addr, int size, int type);
 
     [DllImport("libc.so")]
+    [SupportedOSPlatform(OS.LIN)]
+    [SupportedOSPlatform(OS.MAC)]
     public static extern void mprotect(void* buffer, int size, int mode);
 
     [DllImport("libc.so")]
+    [SupportedOSPlatform(OS.LIN)]
+    [SupportedOSPlatform(OS.MAC)]
     public static extern void posix_memalign(void** buffer, int alignment, int size);
 
     [DllImport("libc.so")]
+    [SupportedOSPlatform(OS.LIN)]
+    [SupportedOSPlatform(OS.MAC)]
     public static extern void free(void* buffer);
 }

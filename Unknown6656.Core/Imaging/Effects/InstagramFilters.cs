4bdc6eb790ab6dfea9c4ab373c69dac9ee49b0fc
unknown6656 <unknown84@gmx.de>
2022-03-27 12:51:18 +0200

    continued adding instagram image filters

--- Unknown6656.Core/Imaging/Effects/InstagramFilters.cs
+++ Unknown6656.Core/Imaging/Effects/InstagramFilters.cs
@@ -16,14 +16,146 @@ using Unknown6656.Mathematics;
 using Unknown6656.Runtime;
 
 using Random = Unknown6656.Mathematics.Numerics.Random;
+using System.Security.Cryptography;
 
 namespace Unknown6656.Imaging.Effects;
 
 
+/// <summary>
+/// A static class containing all known instagram bitmap filters.
+/// </summary>
+/// <completionlist cref="InstagramFilters"/>
 public static class InstagramFilters
 {
-    public static NashvilleFilter Nashville { get; } = new();
+    public static _1977Filter _1977 { get; } = new();
+    public static AdenFilter Aden { get; } = new();
+    public static BrannanFilter Brannan { get; } = new();
+    public static BrooklynFilter Brooklyn { get; } = new();
+    public static ClarendonFilter Clarendon { get; } = new();
+    // public static EarlybirdFilter Earlybird { get; } = new();
+    // public static GinghamFilter Gingham { get; } = new();
+    // public static HudsonFilter Hudson { get; } = new();
+    // public static InkwellFilter Inkwell { get; } = new();
+    // public static KelvinFilter Kelvin { get; } = new();
+    // public static LarkFilter Lark { get; } = new();
+    // public static LofiFilter Lofi { get; } = new();
+    // public static MavenFilter Maven { get; } = new();
+    // public static MayfairFilter Mayfair { get; } = new();
+    // public static MoonFilter Moon { get; } = new();
+    // public static PerpetuaFilter Perpetua { get; } = new();
+    // public static ReyesFilter Reyes { get; } = new();
+    // public static RiseFilter Rise { get; } = new();
+    // public static SlumberFilter Slumber { get; } = new();
+    // public static StinsonFilter Stinson { get; } = new();
+    // public static ToasterFilter Toaster { get; } = new();
+    // public static ValenciaFilter Valencia { get; } = new();
+    // public static WaldenFilter Walden { get; } = new();
+    // public static WillowFilter Willow { get; } = new();
+    // public static XPro2Filter XPro2 { get; } = new();
     public static LegacyNashvilleFilter LegacyNashville { get; } = new();
+}
+
+/// <summary>
+/// Represents the Instagram '1977' bitmap filter.
+/// </summary>
+public sealed class _1977Filter
+    : AcceleratedChainedPartialBitmapEffect
+{
+    public _1977Filter()
+        : base(
+            new ConstantColor(0x4cf36abcu, BlendMode.Screen),
+            new Contrast(1.1),
+            new Brightness(1.1),
+            new Saturation(1.3)
+        )
+    {
+    }
+}
+
+/// <summary>
+/// Represents the Instagram 'Aden' bitmap filter.
+/// </summary>
+public sealed class AdenFilter
+    : ChainedPartialBitmapEffect
+{
+    public AdenFilter()
+        : base(
+            PartialBitmapEffect.FromDelegate((bmp, region) =>
+            {
+                BitmapMask mask = BitmapMask.Radial(bmp.Width, bmp.Height, new()
+                {
+                    EndIntensity = 1,
+                    StartIntensity = .8
+                });
+                Bitmap darkened = bmp.ApplyEffect(new ConstantColor(0xff420a0eu) { Blending = BlendMode.Darken });
+
+                return mask.Composite(darkened, bmp);
+            }),
+            new Hue(-20),
+            new Contrast(.9),
+            new Saturation(.85),
+            new Brightness(1.2)
+        )
+    {
+    }
+}
+
+/// <summary>
+/// Represents the Instagram 'Brooklyn' bitmap filter.
+/// </summary>
+public sealed class BrooklynFilter
+    : ChainedPartialBitmapEffect
+{
+    public BrooklynFilter()
+        : base(
+            PartialBitmapEffect.FromDelegate((bmp, region) =>
+            {
+                BitmapMask mask = BitmapMask.Radial(bmp.Width, bmp.Height, new()
+                {
+                    Radius = .7 * new Vector2(bmp.Width, bmp.Height).Length,
+                });
+                Bitmap b1 = bmp.ApplyEffect(new ConstantColor(0x66A8DFC1) { Blending = BlendMode.Overlay });
+                Bitmap b2 = bmp.ApplyEffect(new ConstantColor(0xFFC4B7C8) { Blending = BlendMode.Overlay });
+
+                return mask.Composite(b1, b2);
+            }),
+            new Contrast(.9),
+            new Brightness(1.1)
+        )
+    {
+    }
+}
+
+/// <summary>
+/// Represents the Instagram 'Brannan' bitmap filter.
+/// </summary>
+public sealed class BrannanFilter
+    : AcceleratedChainedPartialBitmapEffect
+{
+    public BrannanFilter()
+        : base(
+            new ConstantColor(0x4FA12CC7) { Blending = BlendMode.Lighten },
+            new Sepia(.5),
+            new Contrast(1.4)
+        )
+    {
+    }
+}
+
+/// <summary>
+/// Represents the Instagram 'Clarendon' bitmap filter.
+/// </summary>
+public sealed class ClarendonFilter
+    : AcceleratedChainedPartialBitmapEffect
+{
+    public ClarendonFilter()
+        : base(
+            new ConstantColor(0x337FBBE3) { Blending = BlendMode.Overlay },
+            new Contrast(1.2),
+            new Saturation(1.35)
+        )
+    {
+    }
 }
 
 /// <summary>
@@ -34,8 +166,8 @@ public sealed class NashvilleFilter
 {
     public NashvilleFilter()
         : base(
-            new ConstantColor(0x8EF7B099u, BlendMode.Darken),
-            new ConstantColor(0x66004696u, BlendMode.Lighten),
+            new ConstantColor(0x8EF7B099u) { Blending = BlendMode.Darken },
+            new ConstantColor(0x66004696u) { Blending = BlendMode.Lighten },
             new Sepia(.2),
             new Contrast(1.2),
             new Brightness(1.05),
@@ -58,7 +190,7 @@ public sealed class LegacyNashvilleFilter
             new Contrast(1.5),
             new Brightness(.9),
             new Hue(-.261799),
-            new ConstantColor(0x47804e0f, BlendMode.Screen)
+            new ConstantColor(0x47804e0f) { Blending = BlendMode.Screen }
         )
     {
     }

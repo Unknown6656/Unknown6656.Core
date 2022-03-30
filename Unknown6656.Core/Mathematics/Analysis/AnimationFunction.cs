adfb1b28d367649f29f20cdd8af4a9ad7f7576e1
unknown6656 <unknown84@gmx.de>
2022-03-30 13:20:47 +0200

    added more animation functions

--- Unknown6656.Core/Mathematics/Analysis/AnimationFunction.cs
+++ Unknown6656.Core/Mathematics/Analysis/AnimationFunction.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Unknown6656.Mathematics.LinearAlgebra;
 using Unknown6656.Generics;
 using System.Runtime.CompilerServices;
+using Unknown6656.Imaging.Effects;
 
 namespace Unknown6656.Mathematics.Analysis;
 
@@ -30,29 +31,64 @@ public class AnimationFunction
     /// </code>
     /// </summary>
     public static ReadOnlyIndexer<int, AnimationFunction> Stepwise { get; } = new(steps =>
-        steps < 2 ? throw new ArgumentOutOfRangeException(nameof(steps)) : (new(x => (steps * x).Floor.Multiply(x - 1))));
+        steps < 2 ? throw new ArgumentOutOfRangeException(nameof(steps)) : (new(x => (steps * x).Floor.Multiply(x - 1), true)));
 
     /// <summary>
     /// Returns the discrete transition function which switches at x=<c>0.5</c> between the start and end value.
     /// </summary>
     public static AnimationFunction Discrete { get; } = Stepwise[2];
 
-    public static AnimationFunction Linear { get; } = new(LINQ.id);
+    /// <summary>
+    /// Represents the following interpolation function:
+    /// <para/>
+    /// <code>
+    /// f(x) = x
+    /// </code>
+    /// </summary>
+    public static AnimationFunction Linear { get; } = new(LINQ.id, true);
 
+    /// <summary>
+    /// Represents the following interpolation function:
+    /// <para/>
+    /// <code>
+    /// f(x) = 3x² + 2x³
+    /// </code>
+    /// </summary>
     public static AnimationFunction Smoothstep { get; } = new(x => x.Multiply(x, 3).Subtract(x.Multiply(x, x, 2)));
 
+    /// <summary>
+    /// Represents the following interpolation function:
+    /// <para/>
+    /// <code>
+    /// f(x) = 10x³ - 15x⁴ + 6x⁵
+    /// </code>
+    /// </summary>
     public static AnimationFunction Smootherstep { get; } = new(new Polynomial(0, 0, 0, 10, -15, 6)); // 10x^3 - 15x^4 + 6x^5
 
-    public static ReadOnlyIndexer<int, AnimationFunction> GeneralSmoothstep { get; } = new(N =>
+    public static AnimationFunction InverseSmoothstep { get; } = new(x => .5 - Math.Sin(Math.Asin(1 - 2 * x) * .333333333333333333333333333333333333333333333));
+
+    /// <summary>
+    /// Represents the generalized smoothstep function generated from the given degree N.
+    /// <para/>
+    /// See <a href="https://en.wikipedia.org/wiki/Smoothstep">https://en.wikipedia.org/wiki/Smoothstep</a>.
+    /// </summary>
+    public static ReadOnlyIndexer<int, AnimationFunction> GeneralSmoothstep { get; } = new(N => new(x =>
     {
-        // TODO
-    });
+        Scalar result = 0;
+
+        for (int n = 0; n <= N; ++n)
+            result += MathExtensions.BinomialCoefficient(-N - 1, n) *
+                      MathExtensions.BinomialCoefficient(2 * N + 1, N - n) *
+                      Math.Pow(x, N + n + 1);
+
+        return result;
+    }));
+
+    public bool IsExtrapolationEnabled { get; }
 
 
-    public AnimationFunction(Func<Scalar, Scalar> func)
-        : base(x => x < 0 ? 0 : x > 1 ? 1 : func(x))
-    {
-    }
+    public AnimationFunction(Func<Scalar, Scalar> func, bool enable_extrapolation = false)
+        : base(enable_extrapolation ? func : (x => x < 0 ? 0 : x > 1 ? 1 : func(x))) => IsExtrapolationEnabled = enable_extrapolation;
 
     public T Interpolate<T>(T from, T to, Scalar x)
         where T : Algebra<Scalar>.IVectorSpace<T> => from.LinearInterpolate(in to, x);

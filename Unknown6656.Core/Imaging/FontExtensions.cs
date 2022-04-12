f46a62839a11b3687537ec04da9a49ee6ccca5c5
unknown6656 <unknown84@gmx.de>
2022-04-12 22:36:31 +0200

    added placeholder class for font functions

---
+++ Unknown6656.Core/Imaging/FontExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Imaging;
+
+
+public static class FontExtensions
+{
+    // TODO : load font
+    // TODO : list fonts
+    // TODO : install font
+    // TODO : check if monospaced
+    // TODO : render font
+    // TODO : font/text render effect for bitmaps?
+}

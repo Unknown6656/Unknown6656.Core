206df1de529a1a11e2fa9771601d91774528eacb
unknown6656 <unknown84@gmx.de>
2022-04-16 09:23:07 +0200

    finished implementing the core parts of the IL disassembler

--- Unknown6656.Core/Runtime/ILDisassembler.cs
+++ Unknown6656.Core/Runtime/ILDisassembler.cs
@@ -5,19 +5,28 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+using Unknown6656.Generics;
+using Unknown6656.Common;
+
 namespace Unknown6656.Runtime;
 
 
-public static class CILDisassembler
+public static class ILDisassembler
 {
-    public static string Disassemble(MethodInfo method) =>
-        method.GetMethodBody() is { } body ? Disassemble(body) : throw new ArgumentException("The given method does not define a method body.", nameof(method));
+    private static readonly ILSignatureProvider _ilsigprov = new();
 
-    public static string Disassemble(MethodBody method!!)
+
+    public static string Disassemble(MethodInfo method) =>
+        method.GetMethodBody() is { } body ? Disassemble(body, method.Module) : throw new ArgumentException("The given method does not define a method body.", nameof(method));
+
+    public static string Disassemble(MethodBody method!!, Module module)
     {
+        byte[]? signature = module.ResolveSignature(method.LocalSignatureMetadataToken);
         IList<LocalVariableInfo> variables = method.LocalVariables;
         IList<ExceptionHandlingClause> handlers = method.ExceptionHandlingClauses;
         StringBuilder il = new();
+
+        // TODO : do something with 'signature'
 
         il.AppendLine("{");
         il.AppendLine($"    .maxstack {method.MaxStackSize}");
@@ -32,19 +41,13 @@ public static class CILDisassembler
             il.AppendLine($"    )");
         }
 
-        foreach (string line in Disassemble(method?.GetILAsByteArray()))
-            il.AppendLine($"    {line}");
-
+        il.AppendLines(Disassemble(method?.GetILAsByteArray(), module));
         il.AppendLine("}");
 
-
-        // TODO
-
-
-        throw new NotImplementedException();
+        return il.ToString();
     }
 
-    public static unsafe IEnumerable<string> Disassemble(byte[]? bytes)
+    public static unsafe List<string> Disassemble(byte[]? bytes, Module module)
     {
         List<string> lines = new();
 
@@ -53,14 +56,15 @@ public static class CILDisassembler
             {
                 byte* ptr = start;
                 byte* end = start + bytes.Length;
+                int processed = 0;
 
                 while (ptr < end)
                     try
                     {
-                        if (Process(ptr) is (int adv, string instr))
+                        if (Process(ptr, module, processed) is (int adv, string instr))
                         {
                             ptr += adv;
-                            lines.Add(instr);
+                            lines.Add($"IL_{processed:x8}: {instr}");
                         }
                         else
                             break;
@@ -76,15 +80,13 @@ public static class CILDisassembler
         return lines;
     }
 
-    private static string ResolveToken(int token) => $"<0x{token:x8}>"; // TODO
-
-    private static string ResolveTarget(int target) => $"<0x{target:x8}>"; // TODO
-
-    private static string ResolveString(int index) => $"<0x{index:x8}:\"\">"; // TODO
-
-    private static unsafe (int adv, string instruction)? Process(byte* ptr)
+    private static unsafe (int adv, string instruction)? Process(byte* ptr, Module module, int line)
     {
         T get<T>(int offset = 1) where T : unmanaged => *(T*)(ptr + 1);
+        string resolve_target(int target) => $"IL_{target + line:x8}";
+        string resolve_type(int token) => _ilsigprov.GetTypeName(module.ResolveType(token));
+        string resolve_field(int token) => _ilsigprov.GetCallsiteSignature(module.ResolveField(token));
+        string resolve_method(int token) => _ilsigprov.GetCallsiteSignature(module.ResolveMethod(token));
 
         return *ptr switch
         {
@@ -119,46 +121,53 @@ public static class CILDisassembler
             0x1c => (1, "ldc.i4.6"),
             0x1d => (1, "ldc.i4.7"),
             0x1e => (1, "ldc.i4.8"),
-            0x1f => (1, "ldc.i4.s"),
+            0x1f => (2, $"ldc.i4.s {ptr[1]}"),
             0x20 => (5, $"ldc.i4 0x{get<int>():x8}"),
             0x21 => (9, $"ldc.i8 0x{get<long>():x16}"),
             0x22 => (5, $"ldc.r4 {get<float>()}"),
             0x23 => (9, $"ldc.r8 {get<double>()}"),
             0x25 => (1, "dup"),
             0x26 => (1, "pop"),
-            0x27 => (5, $"jmp {ResolveToken(get<int>())}"),
-            0x28 => (5, $"call {ResolveToken(get<int>())}"),
-            0x29 => (5, $"calli {ResolveToken(get<int>())}"),
+            0x27 => (5, $"jmp {resolve_method(get<int>())}"),
+            0x28 => (5, $"call {resolve_method(get<int>())}"),
+            0x29 => (5, $"calli {resolve_method(get<int>())}"),
             0x2a => (1, "ret"),
-            0x2b => (2, $"br.s {ResolveTarget(ptr[1])}"),
-            0x2c => (2, $"brfalse.s {ResolveTarget(ptr[1])}"),
-            0x2d => (2, $"brtrue.s {ResolveTarget(ptr[1])}"),
-            0x2e => (2, $"beq.s {ResolveTarget(ptr[1])}"),
-            0x2f => (2, $"bge.s {ResolveTarget(ptr[1])}"),
-            0x30 => (2, $"bgt.s {ResolveTarget(ptr[1])}"),
-            0x31 => (2, $"ble.s {ResolveTarget(ptr[1])}"),
-            0x32 => (2, $"blt.s {ResolveTarget(ptr[1])}"),
-            0x33 => (2, $"bne.un.s {ResolveTarget(ptr[1])}"),
-            0x34 => (2, $"bge.un.s {ResolveTarget(ptr[1])}"),
-            0x35 => (2, $"bgt.un.s {ResolveTarget(ptr[1])}"),
-            0x36 => (2, $"ble.un.s {ResolveTarget(ptr[1])}"),
-            0x37 => (2, $"blt.un.s {ResolveTarget(ptr[1])}"),
-            0x38 => (5, $"br {ResolveTarget(get<int>())}"),
-            0x39 => (5, $"brfalse {ResolveTarget(get<int>())}"),
-            0x3a => (5, $"brtrue {ResolveTarget(get<int>())}"),
-            0x3b => (5, $"beq {ResolveTarget(get<int>())}"),
-            0x3c => (5, $"bge {ResolveTarget(get<int>())}"),
-            0x3d => (5, $"bgt {ResolveTarget(get<int>())}"),
-            0x3e => (5, $"ble {ResolveTarget(get<int>())}"),
-            0x3f => (5, $"blt {ResolveTarget(get<int>())}"),
-            0x40 => (5, $"beq.un {ResolveTarget(get<int>())}"),
-            0x41 => (5, $"bge.un {ResolveTarget(get<int>())}"),
-            0x42 => (5, $"bgt.un {ResolveTarget(get<int>())}"),
-            0x43 => (5, $"ble.un {ResolveTarget(get<int>())}"),
-            0x44 => (5, $"blt.un {ResolveTarget(get<int>())}"),
+            0x2b => (2, $"br.s {resolve_target(get<sbyte>())}"),
+            0x2c => (2, $"brfalse.s {resolve_target(get<sbyte>())}"),
+            0x2d => (2, $"brtrue.s {resolve_target(get<sbyte>())}"),
+            0x2e => (2, $"beq.s {resolve_target(get<sbyte>())}"),
+            0x2f => (2, $"bge.s {resolve_target(get<sbyte>())}"),
+            0x30 => (2, $"bgt.s {resolve_target(get<sbyte>())}"),
+            0x31 => (2, $"ble.s {resolve_target(get<sbyte>())}"),
+            0x32 => (2, $"blt.s {resolve_target(get<sbyte>())}"),
+            0x33 => (2, $"bne.un.s {resolve_target(get<sbyte>())}"),
+            0x34 => (2, $"bge.un.s {resolve_target(get<sbyte>())}"),
+            0x35 => (2, $"bgt.un.s {resolve_target(get<sbyte>())}"),
+            0x36 => (2, $"ble.un.s {resolve_target(get<sbyte>())}"),
+            0x37 => (2, $"blt.un.s {resolve_target(get<sbyte>())}"),
+            0x38 => (5, $"br {resolve_target(get<int>())}"),
+            0x39 => (5, $"brfalse {resolve_target(get<int>())}"),
+            0x3a => (5, $"brtrue {resolve_target(get<int>())}"),
+            0x3b => (5, $"beq {resolve_target(get<int>())}"),
+            0x3c => (5, $"bge {resolve_target(get<int>())}"),
+            0x3d => (5, $"bgt {resolve_target(get<int>())}"),
+            0x3e => (5, $"ble {resolve_target(get<int>())}"),
+            0x3f => (5, $"blt {resolve_target(get<int>())}"),
+            0x40 => (5, $"beq.un {resolve_target(get<int>())}"),
+            0x41 => (5, $"bge.un {resolve_target(get<int>())}"),
+            0x42 => (5, $"bgt.un {resolve_target(get<int>())}"),
+            0x43 => (5, $"ble.un {resolve_target(get<int>())}"),
+            0x44 => (5, $"blt.un {resolve_target(get<int>())}"),
+            0x45 => LINQ.Do(delegate
+            {
+                uint n = get<uint>();
+                string[] offs = new string[n];
 
-            0x45 => (1, "switch"), /// TODO ///
+                for (int i = 0; i < offs.Length; ++i)
+                    offs[i] = resolve_target(get<int>(1 + sizeof(uint) + i * sizeof(int)));
 
+                return (1 + sizeof(uint) * offs.Length * sizeof(int), $"switch {n} {offs.StringJoin(" ")}");
+            }),
             0x46 => (1, "ldind.i1"),
             0x47 => (1, "ldind.u1"),
             0x48 => (1, "ldind.i2"),
@@ -200,23 +209,23 @@ public static class CILDisassembler
             0x6c => (1, "conv.r8"),
             0x6d => (1, "conv.u4"),
             0x6e => (1, "conv.u8"),
-            0x6f => (5, $"callvirt {ResolveToken(get<int>())}"),
-            0x70 => (5, $"cpobj {ResolveToken(get<int>())}"),
-            0x71 => (5, $"ldobj {ResolveToken(get<int>())}"),
-            0x72 => (5, $"ldstr {ResolveString(get<int>())}"),
-            0x73 => (5, $"newobj {ResolveToken(get<int>())}"),
-            0x74 => (5, $"castclass {ResolveToken(get<int>())}"),
-            0x75 => (5, $"isinst {ResolveToken(get<int>())}"),
+            0x6f => (5, $"callvirt {resolve_method(get<int>())}"),
+            0x70 => (5, $"cpobj {resolve_type(get<int>())}"),
+            0x71 => (5, $"ldobj {resolve_type(get<int>())}"),
+            0x72 => (5, $"ldstr {module.ResolveString(get<int>()).ToCSharpEscaped(true)}"),
+            0x73 => (5, $"newobj {resolve_method(get<int>())}"),
+            0x74 => (5, $"castclass {resolve_type(get<int>())}"),
+            0x75 => (5, $"isinst {resolve_type(get<int>())}"),
             0x76 => (1, "conv.r.un"),
-            0x79 => (5, $"unbox {ResolveToken(get<int>())}"),
+            0x79 => (5, $"unbox {resolve_type(get<int>())}"),
             0x7a => (1, "throw"),
-            0x7b => (5, $"ldfld {ResolveToken(get<int>())}"),
-            0x7c => (5, $"ldflda {ResolveToken(get<int>())}"),
-            0x7d => (5, $"stfld {ResolveToken(get<int>())}"),
-            0x7e => (5, $"ldsfld {ResolveToken(get<int>())}"),
-            0x7f => (5, $"ldsflda {ResolveToken(get<int>())}"),
-            0x80 => (5, $"stsfld {ResolveToken(get<int>())}"),
-            0x81 => (5, $"stobj {ResolveToken(get<int>())}"),
+            0x7b => (5, $"ldfld {resolve_field(get<int>())}"),
+            0x7c => (5, $"ldflda {resolve_field(get<int>())}"),
+            0x7d => (5, $"stfld {resolve_field(get<int>())}"),
+            0x7e => (5, $"ldsfld {resolve_field(get<int>())}"),
+            0x7f => (5, $"ldsflda {resolve_field(get<int>())}"),
+            0x80 => (5, $"stsfld {resolve_field(get<int>())}"),
+            0x81 => (5, $"stobj {resolve_type(get<int>())}"),
             0x82 => (1, "conv.ovf.i1.un"),
             0x83 => (1, "conv.ovf.i2.un"),
             0x84 => (1, "conv.ovf.i4.un"),
@@ -227,10 +236,10 @@ public static class CILDisassembler
             0x89 => (1, "conv.ovf.u8.un"),
             0x8a => (1, "conv.ovf.i.un"),
             0x8b => (1, "conv.ovf.u.un"),
-            0x8c => (5, $"box {ResolveToken(get<int>())}"),
-            0x8d => (5, $"newarr {ResolveToken(get<int>())}"),
+            0x8c => (5, $"box {resolve_type(get<int>())}"),
+            0x8d => (5, $"newarr {resolve_type(get<int>())}"),
             0x8e => (1, "ldlen"),
-            0x8f => (5, $"ldelema {ResolveToken(get<int>())}"),
+            0x8f => (5, $"ldelema {resolve_type(get<int>())}"),
             0x90 => (1, "ldelem.i1"),
             0x91 => (1, "ldelem.u1"),
             0x92 => (1, "ldelem.i2"),
@@ -250,9 +259,9 @@ public static class CILDisassembler
             0xa0 => (1, "stelem.r4"),
             0xa1 => (1, "stelem.r8"),
             0xa2 => (1, "stelem.ref"),
-            0xa3 => (5, $"ldelem {ResolveToken(get<int>())}"),
-            0xa4 => (5, $"stelem {ResolveToken(get<int>())}"),
-            0xa5 => (5, $"unbox.any {ResolveToken(get<int>())}"),
+            0xa3 => (5, $"ldelem {resolve_type(get<int>())}"),
+            0xa4 => (5, $"stelem {resolve_type(get<int>())}"),
+            0xa5 => (5, $"unbox.any {resolve_type(get<int>())}"),
             0xb3 => (1, "conv.ovf.i1"),
             0xb4 => (1, "conv.ovf.u1"),
             0xb5 => (1, "conv.ovf.i2"),
@@ -261,10 +270,10 @@ public static class CILDisassembler
             0xb8 => (1, "conv.ovf.u4"),
             0xb9 => (1, "conv.ovf.i8"),
             0xba => (1, "conv.ovf.u8"),
-            0xc2 => (5, $"refanyval {ResolveToken(get<int>())}"),
+            0xc2 => (5, $"refanyval {resolve_type(get<int>())}"),
             0xc3 => (1, "ckfinite"),
-            0xc6 => (5, $"mkrefany {ResolveToken(get<int>())}"),
-            0xd0 => (5, $"ldtoken {ResolveToken(get<int>())}"),
+            0xc6 => (5, $"mkrefany {resolve_type(get<int>())}"),
+            0xd0 => (5, $"ldtoken 0x{get<int>():x8}"),
             0xd1 => (1, "conv.u2"),
             0xd2 => (1, "conv.u1"),
             0xd3 => (1, "conv.i"),
@@ -277,8 +286,8 @@ public static class CILDisassembler
             0xda => (1, "sub.ovf"),
             0xdb => (1, "sub.ovf.un"),
             0xdc => (1, "endfinally"),
-            0xdd => (5, $"leave {ResolveTarget(get<int>())}"),
-            0xde => (2, $"leave.s {ResolveTarget(ptr[1])}"),
+            0xdd => (5, $"leave {resolve_target(get<int>())}"),
+            0xde => (2, $"leave.s {resolve_target(get<sbyte>())}"),
             0xdf => (1, "stind.i"),
             0xe0 => (1, "conv.u"),
             0xfe when ptr[1] is 0x00 => (1, "arglist"),
@@ -287,31 +296,30 @@ public static class CILDisassembler
             0xfe when ptr[1] is 0x03 => (1, "cgt.un"),
             0xfe when ptr[1] is 0x04 => (1, "clt"),
             0xfe when ptr[1] is 0x05 => (1, "clt.un"),
-            0xfe when ptr[1] is 0x06 => (5, $"ldftn {ResolveToken(get<int>())}"),
-            0xfe when ptr[1] is 0x07 => (5, $"ldvirtftn {ResolveToken(get<int>())}"),
+            0xfe when ptr[1] is 0x06 => (5, $"ldftn {resolve_method(get<int>())}"),
+            0xfe when ptr[1] is 0x07 => (5, $"ldvirtftn {resolve_method(get<int>())}"),
             0xfe when ptr[1] is 0x09 => (3, $"ldarg {get<ushort>()}"),
             0xfe when ptr[1] is 0x0a => (3, $"ldarga {get<ushort>()}"),
             0xfe when ptr[1] is 0x0b => (3, $"starg {get<ushort>()}"),
             0xfe when ptr[1] is 0x0c => (3, $"ldloc {get<ushort>()}"),
             0xfe when ptr[1] is 0x0d => (3, $"ldloca {get<ushort>()}"),
-            0xfe when ptr[1] is 0x0e => (5, $"stloc {ResolveToken(get<int>())}"),
+            0xfe when ptr[1] is 0x0e => (3, $"stloc {get<ushort>()}"),
             0xfe when ptr[1] is 0x0f => (1, "localloc"),
             0xfe when ptr[1] is 0x11 => (1, "endfilter"),
             0xfe when ptr[1] is 0x12 => (2, $"unaligned. {ptr[1]}"),
             0xfe when ptr[1] is 0x13 => (1, "volatile."),
             0xfe when ptr[1] is 0x14 => (1, "tail."),
-            0xfe when ptr[1] is 0x15 => (5, $"initobj {ResolveToken(get<int>())}"),
-            0xfe when ptr[1] is 0x16 => (5, $"constrained. {ResolveToken(get<int>())}"),
+            0xfe when ptr[1] is 0x15 => (5, $"initobj {resolve_type(get<int>())}"),
+            0xfe when ptr[1] is 0x16 => (5, $"constrained. {resolve_type(get<int>())}"),
             0xfe when ptr[1] is 0x17 => (1, "cpblk"),
             0xfe when ptr[1] is 0x18 => (1, "initblk"),
             0xfe when ptr[1] is 0x19 => (2, $"no.{((ptr[1] & 1) != 0 ? " typecheck" : "") +
                                                   ((ptr[1] & 2) != 0 ? " rangecheck" : "") +
                                                   ((ptr[1] & 4) != 0 ? " nullcheck" : "")}"),
-            0xfe when ptr[1] is 0x1a => return (1, "rethrow"),
-            0xfe when ptr[1] is 0x1c => return (5, $"sizeof {ResolveToken(get<int>())}"),
-            0xfe when ptr[1] is 0x1d => return (1, "refanytype"),
-            0xfe when ptr[1] is 0x1e => return (1, "readonly."),
-
+            0xfe when ptr[1] is 0x1a => (1, "rethrow"),
+            0xfe when ptr[1] is 0x1c => (5, $"sizeof {resolve_type(get<int>())}"),
+            0xfe when ptr[1] is 0x1d => (1, "refanytype"),
+            0xfe when ptr[1] is 0x1e => (1, "readonly."),
 
             0x24 or
             0x77 or
@@ -326,6 +334,6 @@ public static class CILDisassembler
             0xfe or
             0xff or
             _ => (1, $"<undefined opcode 0x{*ptr:x2}>"),
-        }
+        };
     }
 }

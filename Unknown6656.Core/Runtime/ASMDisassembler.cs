2bb25cc5ba6fe87adef10ed26dd075dd05ba3e2e
unknown6656 <unknown84@gmx.de>
2022-04-16 09:30:37 +0200

    added the emtpy class for the ASM disassembler

---
+++ Unknown6656.Core/Runtime/ASMDisassembler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Runtime;
+
+
+public class ASMDisassembler
+{
+    public static unsafe List<string> Disassemble(byte[]? bytes)
+    {
+        fixed (byte* ptr = bytes)
+            return Disassemble(ptr, bytes.Length);
+    }
+
+    public static unsafe List<string> Disassemble(void* ptr, int count)
+    {
+        List<string> instructions = new();
+
+        throw new NotImplementedException();
+
+        return instructions;
+    }
+}

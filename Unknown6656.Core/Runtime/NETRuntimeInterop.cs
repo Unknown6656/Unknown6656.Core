c0afe733e5ddff116d2726bc6dcabf052627eac9
unknown6656 <unknown84@gmx.de>
2022-04-23 20:30:23 +0200

    began adding .NET runtime interop functions

---
+++ Unknown6656.Core/Runtime/NETRuntimeInterop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Runtime;
+
+
+public static unsafe class NETRuntimeInterop
+{
+    public static void* GetHeapPointer(object? @object) => @object is null ? (void*)null : *(void**)Unsafe.AsPointer(ref @object);
+}

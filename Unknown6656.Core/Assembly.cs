38c6a95511aeba9458a6b6516b174fab10f090b7
unknown6656 <unknown84@gmx.de>
2022-04-30 12:13:24 +0200

    added module initializer code

--- Unknown6656.Core/Assembly.cs
+++ Unknown6656.Core/Assembly.cs
@@ -1,5 +1,19 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System;
 
+using Unknown6656.Runtime;
 
 [assembly: CLSCompliant(false)]
 
+
+internal class __module
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    [ModuleInitializer]
+    public static void Initializer()
+    {
+        if (!LibGDIPlusInstaller.IsGDIInstalled)
+            LibGDIPlusInstaller.TryInstallGDI();
+    }
+#pragma warning restore CA2255
+}

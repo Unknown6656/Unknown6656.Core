0d04a935d7e62d807da2fc970322af0e3ea0b4ff
unknown6656 <unknown84@gmx.de>
2022-04-30 17:01:30 +0200

    improved the error message

--- Unknown6656.Core/Imaging/BitmapLocker.cs
+++ Unknown6656.Core/Imaging/BitmapLocker.cs
@@ -50,7 +50,7 @@ public unsafe class BitmapLocker
         BitmapData dat = Bitmap.LockBits(new Rectangle(0, 0, Bitmap.Width, Bitmap.Height), ImageLockMode.ReadWrite, Bitmap.PixelFormat);
 
         if (dat.Stride / dat.Width != sizeof(T))
-            throw new InvalidOperationException($"A bitmap with the pixel format {Bitmap.PixelFormat} cannot be locked using a struct of the type '{typeof(T)}' as the sizes do not match ('{typeof(T)}' must have a size of {dat.Stride / dat.Width} bytes, however, it is {sizeof(T)} bytes large).");
+            throw new InvalidOperationException($"A bitmap with the pixel format {Bitmap.PixelFormat} cannot be locked using a struct of the type '{typeof(T)}' as the sizes do not match ('{typeof(T)}' must have a size of {dat.Stride / dat.Width} bytes, however, it is {sizeof(T)} bytes large). You may resolve this issue by applying '{nameof(BitmapExtensions.ToARGB32)}' or '{nameof(BitmapExtensions.ToRGB24)}' (inside the class '{typeof(BitmapExtensions)}') to the given bitmap before calling this method.");
 
         try
         {

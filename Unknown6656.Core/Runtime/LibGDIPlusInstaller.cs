ea1ecd44ff4f39d6612974f78f6ef1a39a3663e6
unknown6656 <unknown84@gmx.de>
2022-04-30 12:13:15 +0200

    added dummy libgdiplus installer

---
+++ Unknown6656.Core/Runtime/LibGDIPlusInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Runtime;
+
+
+public static class LibGDIPlusInstaller
+{
+    public static bool IsGDIInstalled => false;
+
+    public static void TryInstallGDI()
+    {
+        // TODO
+    }
+}

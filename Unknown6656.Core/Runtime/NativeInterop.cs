4e1e5370d7d8533ebecec076dee182d0fb32e91d
unknown6656 <unknown84@gmx.de>
2022-08-22 07:42:52 +0200

    cleaned up the nativeinterop class

--- Unknown6656.Core/Runtime/NativeInterop.cs
+++ Unknown6656.Core/Runtime/NativeInterop.cs
@@ -3,75 +3,103 @@ using System.Runtime.Versioning;
 using System.Text;
 
 using Unknown6656.Controls.Console;
+using Unknown6656.Imaging;
 
 namespace Unknown6656.Runtime;
 
 
 internal static unsafe class NativeInterop
 {
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-    [SupportedOSPlatform(OS.WIN)]
-    public static extern int GetShortPathName([MarshalAs(UnmanagedType.LPWStr)] string? path, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder @short, int length);
+    public const string GDI32 = "gdi32.dll";
+    public const string USER32 = "user32.dll";
+    public const string KERNEL32 = "kernel32.dll";
+    public const string NTDLL = "ntdll.dll";
+    public const string LIBC = "libc.so";
 
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-    [SupportedOSPlatform(OS.WIN)]
-    public static extern bool CreateHardLink([MarshalAs(UnmanagedType.LPWStr)] string lpFileName, [MarshalAs(UnmanagedType.LPWStr)] string lpExistingFileName, void* reserved);
 
-    [DllImport("gdi32.dll", CharSet = CharSet.Auto, EntryPoint = "GetCurrentObject", ExactSpelling = true, SetLastError = true)]
+    [DllImport(USER32)]
+    [SupportedOSPlatform(OS.WIN)]
+    public extern static int GetDC(void* hwnd);
+
+    [DllImport(USER32)]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern bool GetClientRect(void* hWnd, out RECT lpRect);
+
+    [DllImport(USER32)]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern bool GetWindowRect(void* hWnd, out RECT lpRect);
+
+
+    [DllImport(GDI32, CharSet = CharSet.Auto, EntryPoint = "GetCurrentObject", ExactSpelling = true, SetLastError = true)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern nint IntGetCurrentObject(HandleRef hDC, nint uObjectType);
 
-    [DllImport("kernel32.dll", SetLastError = true)]
+
+    [DllImport(KERNEL32, CharSet = CharSet.Unicode)]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern int GetShortPathName([MarshalAs(UnmanagedType.LPWStr)] string? path, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder @short, int length);
+
+    [DllImport(KERNEL32, CharSet = CharSet.Unicode)]
+    [SupportedOSPlatform(OS.WIN)]
+    public static extern bool CreateHardLink([MarshalAs(UnmanagedType.LPWStr)] string lpFileName, [MarshalAs(UnmanagedType.LPWStr)] string lpExistingFileName, void* reserved);
+
+    [DllImport(KERNEL32, SetLastError = true)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern void* GetStdHandle(int nStdHandle);
 
-    [DllImport("kernel32.dll")]
+    [DllImport(KERNEL32)]
+    [SupportedOSPlatform(OS.WIN)]
+    public extern static void* GetConsoleWindow();
+
+    [DllImport(KERNEL32)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern bool SetConsoleMode(void* hWnd, ConsoleMode dwMode);
 
-    [DllImport("kernel32.dll")]
+    [DllImport(KERNEL32)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern bool GetConsoleMode(void* hWnd, ConsoleMode* lpMode);
 
     [return: MarshalAs(UnmanagedType.Bool)]
-    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport(KERNEL32, SetLastError = true, CharSet = CharSet.Unicode)]
     public static extern bool SetCurrentConsoleFontEx(void* hWnd, bool MaximumWindow, ref ConsoleFontInfo ConsoleCurrentFontEx);
 
     [return: MarshalAs(UnmanagedType.Bool)]
-    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport(KERNEL32, SetLastError = true, CharSet = CharSet.Unicode)]
     public static extern bool GetCurrentConsoleFontEx(void* hWnd, bool MaximumWindow, ref ConsoleFontInfo ConsoleCurrentFontEx);
 
-    [DllImport("kernel32.dll")]
+    [DllImport(KERNEL32)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern void* VirtualAlloc(void* addr, int size, int type, int protect);
 
-    [DllImport("kernel32.dll")]
+    [DllImport(KERNEL32)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern bool VirtualProtect(void* addr, int size, int new_protect, int* old_protect);
 
-    [DllImport("kernel32.dll")]
+    [DllImport(KERNEL32)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern bool VirtualFree(void* addr, int size, int type);
 
-    [DllImport("ntdll.dll")]
+
+    [DllImport(NTDLL)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern int RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool _);
 
-    [DllImport("ntdll.dll")]
+    [DllImport(NTDLL)]
     [SupportedOSPlatform(OS.WIN)]
     public static extern int NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, void* Parameters, uint ValidResponseOption, out uint _);
 
-    [DllImport("libc.so")]
+
+    [DllImport(LIBC)]
     [SupportedOSPlatform(OS.LIN)]
     [SupportedOSPlatform(OS.MAC)]
     public static extern void mprotect(void* buffer, int size, int mode);
 
-    [DllImport("libc.so")]
+    [DllImport(LIBC)]
     [SupportedOSPlatform(OS.LIN)]
     [SupportedOSPlatform(OS.MAC)]
     public static extern void posix_memalign(void** buffer, int alignment, int size);
 
-    [DllImport("libc.so")]
+    [DllImport(LIBC)]
     [SupportedOSPlatform(OS.LIN)]
     [SupportedOSPlatform(OS.MAC)]
     public static extern void free(void* buffer);

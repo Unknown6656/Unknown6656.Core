62b16545d46e9bb663868bf8620a0ad1f0ae72c7
unknown6656 <unknown84@gmx.de>
2022-09-04 19:51:33 +0200

    switched to file-scoped namespace

--- Unknown6656.Core/Mathematics/Cryptography/TextbookRSA.cs
+++ Unknown6656.Core/Mathematics/Cryptography/TextbookRSA.cs
@@ -2,76 +2,74 @@
 
 using Unknown6656.Mathematics.LinearAlgebra;
 
-using bint = System.Numerics.BigInteger;
+namespace Unknown6656.Mathematics.Cryptography;
 
-namespace Unknown6656.Mathematics.Cryptography
+
+public unsafe class TextbookRSA
 {
-    public unsafe class TextbookRSA
+    public static byte[] Encrypt(ModuloRing @public, byte[] M)
     {
-        public static byte[] Encrypt(ModuloRing @public, byte[] M)
+        byte[] C = new byte[M.Length * 4];
+
+        fixed (byte* pc = C)
         {
-            byte[] C = new byte[M.Length * 4];
+            int* ptr = (int*)pc;
 
-            fixed (byte* pc = C)
-            {
-                int* ptr = (int*)pc;
-
-                Parallel.For(0, M.Length, i => ptr[i] = Encrypt(@public, M[i]));
-            }
-
-            return C;
+            Parallel.For(0, M.Length, i => ptr[i] = Encrypt(@public, M[i]));
         }
 
-        public static byte[] Decrypt(ModuloRing @private, byte[] C)
+        return C;
+    }
+
+    public static byte[] Decrypt(ModuloRing @private, byte[] C)
+    {
+        byte[] M = new byte[C.Length / 4];
+
+        fixed (byte* pc = C)
         {
-            byte[] M = new byte[C.Length / 4];
+            int* ptr = (int*)pc;
 
-            fixed (byte* pc = C)
-            {
-                int* ptr = (int*)pc;
-
-                Parallel.For(0, M.Length, i => M[i] = Decrypt(@private, ptr[i]));
-            }
-
-            return M;
+            Parallel.For(0, M.Length, i => M[i] = Decrypt(@private, ptr[i]));
         }
 
-        private static int Encrypt(ModuloRing @public, byte M) => (int)bint.ModPow(M + 2, @public.Value, @public.Modulus);
+        return M;
+    }
 
-        private static byte Decrypt(ModuloRing @private, int C) => (byte)(bint.ModPow(C, @private.Value, @private.Modulus) - 2);
+    private static int Encrypt(ModuloRing @public, byte M) => (int)bint.ModPow(M + 2, @public.Value, @public.Modulus);
 
-        public static (ModuloRing @public, ModuloRing @private) GenerateKeyPair()
+    private static byte Decrypt(ModuloRing @private, int C) => (byte)(bint.ModPow(C, @private.Value, @private.Modulus) - 2);
+
+    public static (ModuloRing @public, ModuloRing @private) GenerateKeyPair()
+    {
+        static bint random_prime() => MathExtensions._primes[Numerics.Random.XorShift.NextInt(0, MathExtensions._primes.Length)];
+        static (bint N, ModuloRing e) GenPublicKey()
         {
-            static bint random_prime() => MathExtensions._primes[Numerics.Random.XorShift.NextInt(0, MathExtensions._primes.Length)];
-            static (bint N, ModuloRing e) GenPublicKey()
-            {
-                bint P = random_prime();
-                bint Q = P;
+            bint P = random_prime();
+            bint Q = P;
 
-                while (P == Q)
-                    Q = random_prime();
+            while (P == Q)
+                Q = random_prime();
 
-                bint N = P * Q;
-                bint φ = (P - 1) * (Q - 1);
-                bint e = φ;
+            bint N = P * Q;
+            bint φ = (P - 1) * (Q - 1);
+            bint e = φ;
 
-                while ((φ % e) == 0 || (e >= φ))
-                    e = random_prime();
+            while ((φ % e) == 0 || (e >= φ))
+                e = random_prime();
 
-                return (N, (e, φ));
-            }
-
-            ModuloRing e, d;
-            bint N;
-
-            do
-            {
-                (N, e) = GenPublicKey();
-                d = e.Invert();
-            }
-            while (d == e || N < 514 || bint.Log(N, 2) > 30);
-
-            return (e, d);
+            return (N, (e, φ));
         }
+
+        ModuloRing e, d;
+        bint N;
+
+        do
+        {
+            (N, e) = GenPublicKey();
+            d = e.Invert();
+        }
+        while (d == e || N < 514 || bint.Log(N, 2) > 30);
+
+        return (e, d);
     }
 }

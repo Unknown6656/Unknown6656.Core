4100f25c5914618e87378849fa8e9405f2928c94
unknown6656 <unknown84@gmx.de>
2022-09-05 10:54:54 +0200

    switched to file scoped namespaces

--- Unknown6656.Core/Mathematics/Numerics/Sequence.cs
+++ Unknown6656.Core/Mathematics/Numerics/Sequence.cs
@@ -6,110 +6,110 @@ using System;
 
 using Unknown6656.Mathematics.LinearAlgebra;
 
-namespace Unknown6656.Mathematics.Numerics
+namespace Unknown6656.Mathematics.Numerics;
+
+
+public static class Sequences
 {
-    public static class Sequences
+    public static Sequence<bint> AllIntegers => (0, i => i > 0 ? -i : 1 - i);
+
+    public static Sequence<bint> PoitiveIntegers => (0, i => i + 1);
+
+    public static Sequence<bint> NegativeIntegers => (0, i => i - 1);
+
+    public static Sequence<int> Primes => new(MathExtensions._primes);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<bint> Collatz(bint start = default) => (start, i => i.IsEven ? i / 2 : i * 3 + 1);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<double> AllNumbers(double epsilon = double.Epsilon * 2) => (0, s => -s * (1 + (s > 0 ? 0 : epsilon)));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<Scalar> InclusiveRange(Scalar start, Scalar end, uint stepcount) => InclusiveInterval(start, end, (end - start) / stepcount);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<Scalar> InclusiveInterval(Scalar start, Scalar end) => InclusiveInterval(start, end, Scalar.ComputationalEpsilon);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<Scalar> InclusiveInterval(Scalar start, Scalar end, Scalar step)
     {
-        public static Sequence<bint> AllIntegers => (0, i => i > 0 ? -i : 1 - i);
-
-        public static Sequence<bint> PoitiveIntegers => (0, i => i + 1);
-
-        public static Sequence<bint> NegativeIntegers => (0, i => i - 1);
-
-        public static Sequence<int> Primes => new(MathExtensions._primes);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<bint> Collatz(bint start = default) => (start, i => i.IsEven ? i / 2 : i * 3 + 1);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<double> AllNumbers(double epsilon = double.Epsilon * 2) => (0, s => -s * (1 + (s > 0 ? 0 : epsilon)));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<Scalar> InclusiveRange(Scalar start, Scalar end, uint stepcount) => InclusiveInterval(start, end, (end - start) / stepcount);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<Scalar> InclusiveInterval(Scalar start, Scalar end) => InclusiveInterval(start, end, Scalar.ComputationalEpsilon);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<Scalar> InclusiveInterval(Scalar start, Scalar end, Scalar step)
+        IEnumerable<Scalar> iter()
         {
-            IEnumerable<Scalar> iter()
+            yield return start;
+
+            if (!start.Is(end))
             {
-                yield return start;
+                bool asc = start < end;
 
-                if (!start.Is(end))
-                {
-                    bool asc = start < end;
+                if (asc && step <= 0)
+                    throw new ArgumentException("The step size must be greater than zero.", nameof(step));
+                else if (!asc && step >= 0)
+                    throw new ArgumentException("The step size must be smaller than zero.", nameof(step));
 
-                    if (asc && step <= 0)
-                        throw new ArgumentException("The step size must be greater than zero.", nameof(step));
-                    else if (!asc && step >= 0)
-                        throw new ArgumentException("The step size must be smaller than zero.", nameof(step));
-
-                    while (!start.Is(end))
-                        yield return asc ? start += step : start -= step;
-                }
-            }
-
-            return iter().ToSequence();
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<Scalar> OpenInterval(Scalar start, bool ascending = true) => OpenInterval(start, Scalar.ComputationalEpsilon, ascending);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Sequence<Scalar> OpenInterval(Scalar start, Scalar step, bool ascending = true) => (start, i => ascending ? i + step : i - step);
-
-
-        public static Sequence<T> ToSequence<T>(this IEnumerable<T> coll) => new(coll);
-    }
-
-    public class Sequence<T>
-        : IEnumerable<T>
-    {
-        private readonly Func<IEnumerable<T>> _iterator;
-
-        public IEnumerable<T> Items => _iterator();
-
-        public IEnumerable<(int index, T item)> IndexedItems => Items.Zip(Sequences.PoitiveIntegers).Select(t => ((int)t.Second, t.First));
-
-        public T this[int index] => (index switch
-        {
-            0 => Items,
-            > 0 => Items.Skip(index - 1),
-            _ => throw new ArgumentOutOfRangeException(nameof(index))
-        }).First();
-
-
-        public Sequence(IEnumerable<T> enumerable)
-            : this(() => enumerable)
-        {
-        }
-
-        public Sequence(Func<IEnumerable<T>> iterator) => _iterator = iterator;
-
-        public Sequence(T seed, Func<T, T> next)
-            : this(CreateEnumerable(seed, next))
-        {
-        }
-
-        public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => (Items as IEnumerable).GetEnumerator();
-
-        private static IEnumerable<T> CreateEnumerable(T seed, Func<T, T> next)
-        {
-            while (true)
-            {
-                yield return seed;
-
-                seed = next(seed);
+                while (!start.Is(end))
+                    yield return asc ? start += step : start -= step;
             }
         }
 
-
-        public static implicit operator Sequence<T>(Func<IEnumerable<T>> f) => new(f);
-
-        public static implicit operator Sequence<T>((T seed, Func<T, T> next) f) => new(f.seed, f.next);
+        return iter().ToSequence();
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<Scalar> OpenInterval(Scalar start, bool ascending = true) => OpenInterval(start, Scalar.ComputationalEpsilon, ascending);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Sequence<Scalar> OpenInterval(Scalar start, Scalar step, bool ascending = true) => (start, i => ascending ? i + step : i - step);
+
+
+    public static Sequence<T> ToSequence<T>(this IEnumerable<T> coll) => new(coll);
+}
+
+public class Sequence<T>
+    : IEnumerable<T>
+{
+    private readonly Func<IEnumerable<T>> _iterator;
+
+    public IEnumerable<T> Items => _iterator();
+
+    public IEnumerable<(int index, T item)> IndexedItems => Items.Zip(Sequences.PoitiveIntegers).Select(t => ((int)t.Second, t.First));
+
+    public T this[int index] => (index switch
+    {
+        0 => Items,
+        > 0 => Items.Skip(index - 1),
+        _ => throw new ArgumentOutOfRangeException(nameof(index))
+    }).First();
+
+
+    public Sequence(IEnumerable<T> enumerable)
+        : this(() => enumerable)
+    {
+    }
+
+    public Sequence(Func<IEnumerable<T>> iterator) => _iterator = iterator;
+
+    public Sequence(T seed, Func<T, T> next)
+        : this(CreateEnumerable(seed, next))
+    {
+    }
+
+    public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => (Items as IEnumerable).GetEnumerator();
+
+    private static IEnumerable<T> CreateEnumerable(T seed, Func<T, T> next)
+    {
+        while (true)
+        {
+            yield return seed;
+
+            seed = next(seed);
+        }
+    }
+
+
+    public static implicit operator Sequence<T>(Func<IEnumerable<T>> f) => new(f);
+
+    public static implicit operator Sequence<T>((T seed, Func<T, T> next) f) => new(f.seed, f.next);
 }
--- Unknown6656.Core/Mathematics/StackBasedCalculator.cs
+++ Unknown6656.Core/Mathematics/StackBasedCalculator.cs
@@ -7,177 +7,177 @@ using Unknown6656.Generics;
 
 using data = Unknown6656.Union<Unknown6656.Mathematics.Analysis.Complex, System.Numerics.BigInteger>;
 
-namespace Unknown6656.Mathematics
+namespace Unknown6656.Mathematics;
+
+
+[Obsolete]
+public sealed class StackBasedCalculator
 {
-    [Obsolete]
-    public sealed class StackBasedCalculator
+    #region PROPERTIES
+
+    private readonly Stack<data> _stack = new();
+
+    public data[] Stack => _stack.ToArray();
+
+    public data TopMost => _stack.Peek();
+
+    public int Size => _stack.Count;
+
+    #endregion
+    #region STACK METHODS
+
+    public void Swap() => Swap(2);
+
+    public void Swap(int depth)
     {
-        #region PROPERTIES
+        data[] stack = new data[depth];
 
-        private readonly Stack<data> _stack = new();
+        for (int i = 0; i < depth; ++i)
+            stack[i] = Pop();
 
-        public data[] Stack => _stack.ToArray();
+        Push(stack[0]);
 
-        public data TopMost => _stack.Peek();
-
-        public int Size => _stack.Count;
-
-        #endregion
-        #region STACK METHODS
-
-        public void Swap() => Swap(2);
-
-        public void Swap(int depth)
-        {
-            data[] stack = new data[depth];
-
-            for (int i = 0; i < depth; ++i)
-                stack[i] = Pop();
-
-            Push(stack[0]);
-
-            for (int i = depth - 1; i > 0; --i)
-                Push(stack[i]);
-        }
-
-        public void Duplicate() => Push(TopMost);
-
-        public void Push(data v) => _stack.Push(v);
-
-        public void Push(bint b) => Push(b);
-
-        public void Push(Complex c) => Push(c);
-
-        public data Pop() => _stack.Pop();
-
-        public bint PopBigInteger() => Pop().Match(c => c.Real, LINQ.id);
-
-        public Complex PopComplex() => Pop().Match(LINQ.id, c => new((Scalar)(decimal)c));
-
-        private void UnaryOperator(Func<bint, bint> fb, Func<Complex, Complex> fc) => UnaryOperator(d =>
-        {
-            if (d.Is(out bint b))
-                return fb(b);
-            else if (d.Is(out Complex c))
-                return fc(c);
-            else
-                throw new InvalidOperationException($"The current opeation is not defined for a stack element of the type '{d.UnsafeItem?.GetType()}'.");
-        });
-
-        private void UnaryOperator(Func<data, data> op) => Push(op(Pop()));
-
-        private void BinaryOperator(Func<bint, bint, bint> fb, Func<Complex, Complex, Complex> fc) => BinaryOperator((d1, d2) =>
-        {
-            if (d1.Is(out bint b1) && d2.Is(out bint b2))
-                return fb(b1, b2);
-
-            return fc((Complex)d1.UnsafeItem, (Complex)d2.UnsafeItem);
-        });
-
-        private void BinaryOperator(Func<data, data, data> op)
-        {
-            data snd = Pop();
-            data fst = Pop();
-
-            Push(op(fst, snd));
-        }
-
-        private void TernaryOperator(Func<data, data, data, data> op)
-        {
-            data thr = Pop();
-            data snd = Pop();
-            data fst = Pop();
-
-            Push(op(fst, snd, thr));
-        }
-
-        #endregion
-        #region MATHEMATICAL METHODS
-
-        public void CastToBint() => Push(PopBigInteger());
-
-        public void CastToComplex() => Push(PopComplex());
-
-        public void Add() => BinaryOperator(bint.Add, Complex.Add);
-
-        public void Subtract() => BinaryOperator(bint.Subtract, Complex.Subtract);
-
-        public void Multiply() => BinaryOperator(bint.Multiply, Complex.Multiply);
-
-        public void Divide() => BinaryOperator(bint.Divide, Complex.Divide);
-
-        public void Power() => BinaryOperator((b, e) => bint.Pow(b, (int)e), Complex.Power);
-
-        // MOD
-        // ABS
-        // SIGN
-        // DELTA
-        // SIN
-        // COS
-        // TAN
-        // COT
-        // SEC
-        // CSC
-        // ASIN
-        // ACOS
-        // ATAN
-        // ACOT
-        // ASEC
-        // ACSC
-        // SINH
-        // COSH
-        // TANH
-        // COTH
-        // SECH
-        // CSCH
-        // ASINH
-        // ACOSH
-        // ATANH
-        // ACOTH
-        // ASECH
-        // ACSCH
-        // CIS
-        // CONJ
-        // EXP
-        // LOG_e
-        // LOG_2
-        // LOG_10
-        // LOG_<>
-        // IF
-        // BOOL
-        // NOT
-        // AND
-        // OR
-        // SHL
-        // SHR
-        // ROL
-        // ROR
-        // EQ
-        // NEQ
-        // LT
-        // LTE
-        // GT
-        // GTE
-        // RAND
-        // ONE_MINUS
-        // INVERT
-        // NEGATE
-        // FLOOR
-        // CEILING
-        // ROUND
-        // SQRT
-        // ROOT_n
-        // FAC / PLS
-
-        // c: i
-        // c: ZERO
-        // c: ONE
-        // c: PI
-        // c: TAU
-        // c: PHI
-        // c: e
-        // c: SQRT2
-
-        #endregion
+        for (int i = depth - 1; i > 0; --i)
+            Push(stack[i]);
     }
+
+    public void Duplicate() => Push(TopMost);
+
+    public void Push(data v) => _stack.Push(v);
+
+    public void Push(bint b) => Push(b);
+
+    public void Push(Complex c) => Push(c);
+
+    public data Pop() => _stack.Pop();
+
+    public bint PopBigInteger() => Pop().Match(c => c.Real, LINQ.id);
+
+    public Complex PopComplex() => Pop().Match(LINQ.id, c => new((Scalar)(decimal)c));
+
+    private void UnaryOperator(Func<bint, bint> fb, Func<Complex, Complex> fc) => UnaryOperator(d =>
+    {
+        if (d.Is(out bint b))
+            return fb(b);
+        else if (d.Is(out Complex c))
+            return fc(c);
+        else
+            throw new InvalidOperationException($"The current opeation is not defined for a stack element of the type '{d.UnsafeItem?.GetType()}'.");
+    });
+
+    private void UnaryOperator(Func<data, data> op) => Push(op(Pop()));
+
+    private void BinaryOperator(Func<bint, bint, bint> fb, Func<Complex, Complex, Complex> fc) => BinaryOperator((d1, d2) =>
+    {
+        if (d1.Is(out bint b1) && d2.Is(out bint b2))
+            return fb(b1, b2);
+
+        return fc((Complex)d1.UnsafeItem, (Complex)d2.UnsafeItem);
+    });
+
+    private void BinaryOperator(Func<data, data, data> op)
+    {
+        data snd = Pop();
+        data fst = Pop();
+
+        Push(op(fst, snd));
+    }
+
+    private void TernaryOperator(Func<data, data, data, data> op)
+    {
+        data thr = Pop();
+        data snd = Pop();
+        data fst = Pop();
+
+        Push(op(fst, snd, thr));
+    }
+
+    #endregion
+    #region MATHEMATICAL METHODS
+
+    public void CastToBint() => Push(PopBigInteger());
+
+    public void CastToComplex() => Push(PopComplex());
+
+    public void Add() => BinaryOperator(bint.Add, Complex.Add);
+
+    public void Subtract() => BinaryOperator(bint.Subtract, Complex.Subtract);
+
+    public void Multiply() => BinaryOperator(bint.Multiply, Complex.Multiply);
+
+    public void Divide() => BinaryOperator(bint.Divide, Complex.Divide);
+
+    public void Power() => BinaryOperator((b, e) => bint.Pow(b, (int)e), Complex.Power);
+
+    // MOD
+    // ABS
+    // SIGN
+    // DELTA
+    // SIN
+    // COS
+    // TAN
+    // COT
+    // SEC
+    // CSC
+    // ASIN
+    // ACOS
+    // ATAN
+    // ACOT
+    // ASEC
+    // ACSC
+    // SINH
+    // COSH
+    // TANH
+    // COTH
+    // SECH
+    // CSCH
+    // ASINH
+    // ACOSH
+    // ATANH
+    // ACOTH
+    // ASECH
+    // ACSCH
+    // CIS
+    // CONJ
+    // EXP
+    // LOG_e
+    // LOG_2
+    // LOG_10
+    // LOG_<>
+    // IF
+    // BOOL
+    // NOT
+    // AND
+    // OR
+    // SHL
+    // SHR
+    // ROL
+    // ROR
+    // EQ
+    // NEQ
+    // LT
+    // LTE
+    // GT
+    // GTE
+    // RAND
+    // ONE_MINUS
+    // INVERT
+    // NEGATE
+    // FLOOR
+    // CEILING
+    // ROUND
+    // SQRT
+    // ROOT_n
+    // FAC / PLS
+
+    // c: i
+    // c: ZERO
+    // c: ONE
+    // c: PI
+    // c: TAU
+    // c: PHI
+    // c: e
+    // c: SQRT2
+
+    #endregion
 }

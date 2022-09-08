cce9db7ea30fcba836a5a031fc044dae3355bca5
unknown6656 <unknown84@gmx.de>
2022-09-09 00:05:24 +0200

    began adding ANSI and ASCII bitmap renderers

---
+++ Unknown6656.Core/Imaging/Rendering/ANSIRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Unknown6656.Controls.Console;
+
+namespace Unknown6656.Imaging.Rendering;
+
+
+public class ANSIRenderer
+    : Renderer
+{
+    public ANSIRenderer(RenderingOptions options) : base(options) => throw new NotImplementedException();
+    protected override (int width, int height) GetOutputDimensions() => throw new NotImplementedException();
+    protected override void RenderBitmap(RGBAColor[,] colors, RenderingOptions options_override) => throw new NotImplementedException();
+}
---
+++ Unknown6656.Core/Imaging/Rendering/ASCIIRenderer.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System;
+
+using Unknown6656.Controls.Console;
+
+namespace Unknown6656.Imaging.Rendering;
+
+
+public class ASCIIRenderer
+    : Renderer
+{
+    public override ASCIIRenderingOptions Options { get; }
+    public TextWriter Output { get; }
+
+
+    public ASCIIRenderer(TextWriter output, ASCIIRenderingOptions options)
+        : base(options)
+    {
+        Options = options;
+        Output = output;
+    }
+
+    protected override (int width, int height) GetOutputDimensions() => (Options.Width, Options.Height);
+
+    protected override void RenderBitmap(RGBAColor[,] colors, RenderingOptions options_override)
+    {
+        string charset = Options.Charset;
+
+        for (int y = 0, w = colors.GetLength(0), h = colors.GetLength(1); y < h; ++y)
+        {
+            for (int x = 0; x < w; ++x)
+                Output.Write(charset[(int)(colors[x, y].CIEGray * charset.Length)]);
+
+            Output.WriteLine();
+        }
+    }
+}
+
+public class ASCIIConsoleRenderer
+    : ASCIIRenderer
+{
+    private readonly bool _grayscale;
+
+
+    public ASCIIConsoleRenderer(ASCIIRenderingOptions options, bool grayscale = true)
+        : base(Console.Out, options) => _grayscale = grayscale;
+
+    protected override (int width, int height) GetOutputDimensions() => (
+        Math.Min(Math.Min(Console.BufferWidth, Console.WindowWidth), Options.Width < 0 ? short.MaxValue : Options.Width),
+        Math.Min(Math.Min(Console.BufferHeight, Console.WindowHeight), Options.Height < 0 ? short.MaxValue : Options.Height)
+    );
+
+    protected override void RenderBitmap(RGBAColor[,] colors, RenderingOptions options_override)
+    {
+        string charset = Options.Charset;
+
+        for (int y = 0, w = colors.GetLength(0), h = colors.GetLength(1); y < h; ++y)
+        {
+            for (int x = 0; x < w; ++x)
+            {
+                if (!_grayscale)
+                    ConsoleExtensions.RGBForegroundColor = colors[x, y];
+
+                Output.Write(charset[(int)(colors[x, y].CIEGray * charset.Length)]);
+            }
+
+            Output.WriteLine();
+        }
+    }
+}
+
+public record ASCIIRenderingOptions(int Width = 30, int Height = 15)
+    : RenderingOptions
+{
+    /// <summary>
+    /// Ordered by darkness descending
+    /// </summary>
+    public string Charset { get; init; } = "@$%B8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^'`. ";
+}

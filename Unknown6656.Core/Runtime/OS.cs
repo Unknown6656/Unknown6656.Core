bf73a983d657f26f481d48453d1022002c1845a2
Unknown6656 <unknown84@gmx.de>
2024-03-26 19:07:13 +0100

    began adding virtualization detection

--- Unknown6656.Core/Runtime/OS.cs
+++ Unknown6656.Core/Runtime/OS.cs
@@ -1,18 +1,23 @@
 ﻿using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Diagnostics;
+using System.IO;
 
 namespace Unknown6656.Runtime;
 
 
 public static class OS
 {
+    private const string DOCKER_INDICATOR = "/.dockerenv";
+    private const string WSL_INDICATOR = "/proc/sys/fs/binfmt_misc/WSLInterop";
+
     internal const string WIN = "windows";
     internal const string LIN = "linux";
     internal const string IOS = "iOS";
     internal const string MAC = "macos";
     internal const string CAT = "MacCatalyst";
     internal const string AND = "android";
+
 
     public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
@@ -23,6 +28,8 @@ public static class OS
     public static bool IsOSX => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
     public static bool IsPosix => IsWindows | IsOSX | IsFreeBSD;
+
+    public static bool IsInsideDocker => File.Exists(DOCKER_INDICATOR);
 
 
     /// <summary>
@@ -66,5 +73,24 @@ public static class OS
             ExecutBashCommand("echo c > /proc/sysrq-trigger");
         }
 #pragma warning restore CA1416
+    }
+
+    // TODO : check if is running inside a container (container, snap, etc.)
+    // TODO : check if is running inside a VM (vmware, virtualbox, etc.)
+
+
+
+    // TODO : check if the current execution context is WSL
+    public static bool IsInsideWSL
+    {
+        get
+        {
+            if (File.Exists(WSL_INDICATOR))
+                return true;
+
+            // TODO : check "uname -a" for "microsoft" substring.
+
+            return false;
+        }
     }
 }
